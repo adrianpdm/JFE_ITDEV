8b790ee23cf2c5adfe5ca713c83036875825df77
Mobile Dev Sigma <mobiledevsigma@gmail.com>
2020-01-09 04:46:38 +0700

    barebone css

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+    
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-size: 16px;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+  overflow-y: scroll;
+}
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #f5f5f5;
+}
+
+#app {
+  display: block;
+  width: 100%;
+  max-width: 800px;
+  height: 100%;
+  margin: 0 auto;
+  padding: 4rem;
+
+  border-style: solid;
+  border-color: #ddd;
+  border-width: 0px;
+  border-left-width: 1px;
+  border-right-width: 1px;
+
+  background-color: white;
+}
+</style>
