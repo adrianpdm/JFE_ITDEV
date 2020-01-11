1846a19d6588a780fc6900fa66de75ca9622fe50
adrianpdmh <adrian.pdmh@gmail.com>
2020-01-11 12:51:50 +0700

    define input as spec'd

--- src/components/XDateInput.vue
+++ src/components/XDateInput.vue
@@ -1,15 +1,42 @@
 <template>
   <div>
-      
+    <input role="date-input"
+           type="number"
+           :min="1"
+           :max="31">
+    <input role="month-input"
+           type="number"
+           :min="1"
+           :max="12">
+    <input role="year-input"
+           type="text"
+           :min="4"
+           :max="4">
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    label: {
+      type: String
+    },
+    date: {
+      type: [String, Number]
+    },
+    month: {
+      type: [String, Number]
+    },
+    year: {
+      type: [String, Number]
+    },
+    placeholders: {
+      type: Object,
+      default: () => ({})
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
