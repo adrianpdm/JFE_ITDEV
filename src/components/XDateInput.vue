12077ce3c64c9b15f9c88de8f7a278f2ff2f6c09
adrianpdmh <adrian.pdmh@gmail.com>
2020-01-11 13:30:14 +0700

    barebone structur

--- src/components/XDateInput.vue
+++ src/components/XDateInput.vue
@@ -1,17 +1,26 @@
 <template>
-  <div>
-    <input role="date-input"
-           type="number"
-           :min="1"
-           :max="31">
-    <input role="month-input"
-           type="number"
-           :min="1"
-           :max="12">
-    <input role="year-input"
-           type="text"
-           :min="4"
-           :max="4">
+  <div class="x-date-input">
+    <label class="x-date-input__label">
+      {{label}}
+    </label>
+    <div class="x-date-input__input">
+      <input role="date-input"
+             type="number"
+             :min="1"
+             :max="31">
+      <input role="month-input"
+             type="number"
+             :min="1"
+             :max="12">
+      <input role="year-input"
+             type="text"
+             :min="4"
+             :max="4">
+    </div>
+    <p class="x-date-input__hint"
+       :active="errorMsg">
+       {{errorMsg}}
+    </p>
   </div>
 </template>
 
@@ -33,6 +42,19 @@ export default {
     placeholders: {
       type: Object,
       default: () => ({})
+    }
+  },
+  data(){
+    return {
+      errorMsg: '',
+      isFocused: {
+        date: false,
+        month: false,
+        year: false
+      },
+      mDate: null,
+      mMonth: null,
+      mYear: null
     }
   }
 }
