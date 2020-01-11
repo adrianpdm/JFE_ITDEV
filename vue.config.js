b6855b99c2586f254f2443e1ec801608f10b286d
adrianpdmh <adrian.pdmh@gmail.com>
2020-01-12 02:46:11 +0700

    wrong publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === '/JFE_ITDEV/'
-    ? '/my-project/'
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/JFE_ITDEV/'
     : '/'
 }
