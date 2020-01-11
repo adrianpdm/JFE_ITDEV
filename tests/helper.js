c8c1b850edc9d47bf54cdb874d5b73ec0f1ecca2
adrianpdmh <adrian.pdmh@gmail.com>
2020-01-11 07:01:55 +0700

    separate common test function to helper

---
+++ tests/helper.js
@@ -0,0 +1,18 @@
+import { assert, expect } from 'chai'
+
+export function isVueWrapper(wrapper){
+    expect(wrapper.isVueInstance()).to.be.true
+}
+
+export function hasRequiredProps(wrapper, requiredProps) {
+    isVueWrapper(wrapper)
+    Object.keys(requiredProps).forEach(key => {
+        let actual = wrapper.props(key)
+        let expected = requiredProps[key]
+        assert.equal(actual, expected, `has props '${key}'`)
+    })
+}
+
+export default {
+    hasRequiredProps
+}
