706e0281176fcea59dcf4ad0cce14a9aa603af64
adrianpdmh <adrian.pdmh@gmail.com>
2020-01-11 13:24:57 +0700

    add recursive strict equal check fn

--- tests/helper.js
+++ tests/helper.js
@@ -13,12 +13,33 @@ export function hasRequiredProps(wrapper, requiredProps) {
     })
 }
 
+export function hasRequiredState(wrapper, requiredState) {
+    isVueWrapper(wrapper)
+    const $data = typeof wrapper.vm.$data === 'function'
+        ? wrapper.vm.$data()
+        : wrapper.vm.$data
+
+    const check = (actual, expected, rootKey = '') => {
+        Object.keys(expected).forEach(key => {
+            if (!!expected[key] && typeof expected[key] === 'object') {
+                check(actual[key], expected[key], key)
+            } else {
+                assert.strictEqual(actual[key], expected[key])
+            }
+        })
+    }
+
+    check($data, requiredState);
+}
+
 export function isDataSetupAsFunction(wrapper) {
+    isVueWrapper(wrapper)
     expect(typeof wrapper.vm.$options.data === 'function')
         .to.be.true
 }
 
 export default {
     hasRequiredProps,
-    isDataSetupAsFunction
+    isDataSetupAsFunction,
+    hasRequiredState
 }
