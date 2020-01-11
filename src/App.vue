38a8265a348c3194bf39fda3db03eaed4583e8ce
adrianpdmh <adrian.pdmh@gmail.com>
2020-01-12 05:16:57 +0700

    add mockup progress

--- src/App.vue
+++ src/App.vue
@@ -71,6 +71,37 @@
            style="word-break: break-all; overflow: hidden; white-space: pre-wrap;">
       </pre>
     </div>
+    <transition name="fade">
+      <div v-if="isSendingFormData"
+           id="mock-progress">
+        <div id="progress-modal">
+          <div v-if="!hasFinishedSending"
+               class="dots-loader">
+            <i class="dot"></i>
+            <i class="dot"></i>
+            <i class="dot"></i>
+          </div>
+          <template v-else>
+            <h3>
+              Pendaftaran Berhasil!
+            </h3>
+            <br>
+            <div>
+              <button class="btn-action"
+                      @click="onCloseModal"
+                      style="font-size: 0.85em;">
+                <b>OK</b>
+              </button>
+              <button class="btn-action"
+                      @click="() => {onCreateNewForm(); onCloseModal(); }"
+                      style="font-size: 0.85em;">
+                <b>Keluar dan Hapus Data</b>
+              </button>
+            </div>
+          </template>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -141,6 +172,8 @@ export default {
       jobHistory: [],
 
       formId: 1,
+      isSendingFormData: false,
+      hasFinishedSending: false,
 
       stringified: '',
       stringifyInterval: null,
@@ -168,7 +201,22 @@ export default {
         JSON.parse(JSON.stringify(this.jobHistoryTemplate))
       ]
     },
+    hasUnsavedChanges() {
+      return this.name
+        || this.gender
+        || this.address
+        || Object.keys(this.birthDate).every(key => !!this.birthDate[key])
+        || this.jobHistory.every(item => Object.keys(item).every(key => !!item[key]))
+    },
     onCreateNewForm(animate = true) {
+      let proceed = true
+      if (!this.isSendingFormData && this.hasUnsavedChanges()) {
+        proceed = confirm('Yakin untuk menghapus data?')
+      }
+      if (!proceed) {
+        return
+      }
+
       if (animate) {
         this.formId++;
       }
@@ -210,8 +258,18 @@ export default {
             setTimeout(() => {
               alert('Lengkapi seluruh isian terlebih dahulu sebelum mengirimkan data')
             }, 400)
+          } else {
+            this.isSendingFormData = true
+            this.hasFinishedSending = false
+            setTimeout(() => {
+              this.hasFinishedSending = true
+            }, 3000)
           }
         })
+    },
+    onCloseModal() {
+      this.isSendingFormData = false
+      this.hasFinishedSending = false
     }
   },
   watch: {
@@ -237,6 +295,90 @@ export default {
 @media screen and (max-width: 619px) {
   .btn-action.action-new {
     order: 1;
+  }
+}
+
+#mock-progress {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.15);
+}
+
+#progress-modal {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+
+  width: 80vw;
+  max-width: 400px;
+  height: 50vh;
+  max-height: 250px;
+
+  margin: 0 auto;
+  border-radius: 1rem;
+
+  box-shadow: 0 0.25rem 1rem 0 rgba(0, 0, 0, 0.15);
+  background-color: white;
+}
+
+.dots-loader {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  > .dot {
+    position: relative;
+    display: block;
+    width: 1rem;
+    height: 1rem;
+    margin: 0.5rem;
+    border-radius: 9999px;
+    background-color: rgb(30, 115, 220);
+    animation: bounce 0.75s infinite linear;
+
+    &:nth-child(1) {
+      animation-delay: 0;
+    }
+    &:nth-child(2) {
+      animation-delay: 0.2s;
+    }
+    &:nth-child(3) {
+      animation-direction: reverse;
+    }
+  }
+}
+
+@keyframes bounce {
+  0% {
+    transform: translateY(0rem);
+  }
+  33% {
+    transform: translateY(0.5rem);
+  }
+  66% {
+    transform: translateY(-0.25rem);
+  }
+  100% {
+    transform: translateY(0rem);
+  }
+}
+
+.fade {
+  &-enter,
+  &-leave-to {
+    opacity: 0;
+  }
+
+  &-enter-active,
+  &-leave-active {
+    transition: all 0.3s ease-out;
   }
 }
 </style>
