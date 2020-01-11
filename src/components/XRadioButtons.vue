e3603257f2eb3414f18916fe1f77ccbd6da50de7
adrianpdmh <adrian.pdmh@gmail.com>
2020-01-11 18:18:18 +0700

    barebone structure

--- src/components/XRadioButtons.vue
+++ src/components/XRadioButtons.vue
@@ -1,15 +1,42 @@
 <template>
   <div class="x-radio__buttons">
-
+    <template v-for="(opt, index) in options">
+      <input type="radio"
+             :name="name"
+             :key="`radio:${index}`">
+      <label :key="`label:${index}`">
+        {opt{}}
+      </label>
+    </template>
   </div>
 </template>
 
 <script>
 export default {
-    
+  props: {
+    label: {
+      type: String
+    },
+    name: {
+      type: String
+    },
+    value: {
+      type: [String, Number]
+    },
+    options: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      mValue: null,
+      errorMsg: null,
+      isFocused: false
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
