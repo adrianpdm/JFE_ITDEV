014e32203c57487cf8ee2ee5230bce79a45ca78b
adrianpdmh <adrian.pdmh@gmail.com>
2020-01-12 02:42:17 +0700

    remove unexpected line

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
